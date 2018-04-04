0b0d7419ba65d1e201c62987e6c11457689b1f15
Huyen Ha <hathehuyen@gmail.com>
2018-04-04 20:17:22 +0700

    Convert to class

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,99 +3,61 @@ import settings
 import logging
 import time
 
-# set logging time to GMT
-logging.Formatter.converter = time.gmtime
-# create logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.WARN)
 
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-fh = logging.FileHandler('diginetbot.log')
-fh.setLevel(logging.WARN)
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+class BackEnd(object):
+    def __init__(self, logger=None):
+        self.logger = logger or logging.getLogger(__name__)
+        self.diginet = ccxt.acx({'apiKey': settings.diginet_key, 'secret': settings.diginet_secret,
+                            'urls': {'extension': '.json', 'api': 'https://trade.diginet.io'}})
+        self.bitstamp = ccxt.bitstamp({'apiKey': settings.bitstamp_key, 'secret': settings.bitstamp_secret})
 
-# add formatter to ch
-ch.setFormatter(formatter)
-fh.setFormatter(formatter)
+    def get_buy_price_and_quantity(self, orderbook):
+        buys = orderbook['asks']
+        return buys[0][0], buys[0][1]
 
-# add ch to logger
-logger.addHandler(ch)
-logger.addHandler(fh)
+    def get_sell_price_and_quantity(self, orderbook):
+        sells = orderbook['bids']
+        return sells[0][0], sells[0][1]
 
-diginet = ccxt.acx({'apiKey': settings.diginet_key, 'secret': settings.diginet_secret,
-                    'urls': {'extension': '.json', 'api': 'https://trade.diginet.io'}})
-bitstamp = ccxt.bitstamp({'apiKey': settings.bitstamp_key, 'secret': settings.bitstamp_secret})
+    def compare_prices(self, bitstamp_orderbook, diginet_oderbook):
+        bitstamp_buy_price, bitstamp_buy_quantity = self.get_buy_price_and_quantity(bitstamp_orderbook)
+        bitstamp_sell_price, bitstamp_sell_quantity = self.get_sell_price_and_quantity(bitstamp_orderbook)
+        diginet_buy_price, diginet_buy_quantity = self.get_buy_price_and_quantity(diginet_oderbook)
+        diginet_sell_price, diginet_sell_quantity = self.get_sell_price_and_quantity(diginet_oderbook)
+        self.logger.info('Bitstamp sell price ' + str(bitstamp_sell_price * settings.usd_vnd_rate) +
+                    ' - Diginet buy price ' + str(diginet_sell_price) +
+                    ' - Diff: ' +
+                    str((bitstamp_sell_price * settings.usd_vnd_rate - diginet_buy_price) / diginet_buy_price * 100) +
+                    '%')
+        self.logger.info('Diginet sell price ' + str(diginet_sell_price) +
+                    ' - Bitstamp buy price ' + str(bitstamp_buy_price * settings.usd_vnd_rate) +
+                    ' - Diff: ' +
+                    str((diginet_sell_price / settings.usd_vnd_rate - bitstamp_buy_price) / bitstamp_buy_price * 100) +
+                    '%')
+        if (bitstamp_sell_price * settings.usd_vnd_rate - diginet_buy_price) / diginet_buy_price > settings.diff_pct:
+            self.logger.warning('Buy on diginet ' + str(diginet_buy_price) +
+                           ' -> sell on bitstamp ' + str(bitstamp_sell_price * settings.usd_vnd_rate) +
+                           ' - Diff: ' +
+                           str((bitstamp_sell_price * settings.usd_vnd_rate - diginet_buy_price) / diginet_buy_price * 100)
+                           + '%')
+        if (diginet_sell_price / settings.usd_vnd_rate - bitstamp_buy_price) / bitstamp_buy_price > settings.diff_pct:
+            self.logger.warning('Buy on bitstamp ' + str(bitstamp_buy_price * settings.usd_vnd_rate) +
+                           ' -> sell on diginet ' + str(diginet_sell_price) +
+                           ' - Diff: ' +
+                           str((diginet_sell_price / settings.usd_vnd_rate - bitstamp_buy_price) / bitstamp_buy_price * 100)
+                           + '%')
 
-
-def get_buy_price_and_quantity(orderbook):
-    buys = orderbook['asks']
-    return buys[0][0], buys[0][1]
-
-
-def get_sell_price_and_quantity(orderbook):
-    sells = orderbook['bids']
-    return sells[0][0], sells[0][1]
-
-
-def compare_prices(bitstamp_orderbook, diginet_oderbook):
-    bitstamp_buy_price, bitstamp_buy_quantity = get_buy_price_and_quantity(bitstamp_orderbook)
-    bitstamp_sell_price, bitstamp_sell_quantity = get_sell_price_and_quantity(bitstamp_orderbook)
-    diginet_buy_price, diginet_buy_quantity = get_buy_price_and_quantity(diginet_oderbook)
-    diginet_sell_price, diginet_sell_quantity = get_sell_price_and_quantity(diginet_oderbook)
-    logger.info('Bitstamp sell price ' + str(bitstamp_sell_price * settings.usd_vnd_rate) +
-                ' - Diginet buy price ' + str(diginet_sell_price) +
-                ' - Diff: ' +
-                str((bitstamp_sell_price * settings.usd_vnd_rate - diginet_buy_price) / diginet_buy_price * 100) +
-                '%')
-    logger.info('Diginet sell price ' + str(diginet_sell_price) +
-                ' - Bitstamp buy price ' + str(bitstamp_buy_price * settings.usd_vnd_rate) +
-                ' - Diff: ' +
-                str((diginet_sell_price / settings.usd_vnd_rate - bitstamp_buy_price) / bitstamp_buy_price * 100) +
-                '%')
-    if (bitstamp_sell_price * settings.usd_vnd_rate - diginet_buy_price) / diginet_buy_price > settings.diff_pct:
-        logger.warning('Buy on diginet ' + str(diginet_buy_price) +
-                       ' -> sell on bitstamp ' + str(bitstamp_sell_price * settings.usd_vnd_rate) +
-                       ' - Diff: ' +
-                       str((bitstamp_sell_price * settings.usd_vnd_rate - diginet_buy_price) / diginet_buy_price * 100)
-                       + '%')
-    if (diginet_sell_price / settings.usd_vnd_rate - bitstamp_buy_price) / bitstamp_buy_price > settings.diff_pct:
-        logger.warning('Buy on bitstamp ' + str(bitstamp_buy_price * settings.usd_vnd_rate) +
-                       ' -> sell on diginet ' + str(diginet_sell_price) +
-                       ' - Diff: ' +
-                       str((diginet_sell_price / settings.usd_vnd_rate - bitstamp_buy_price) / bitstamp_buy_price * 100)
-                       + '%')
-
-
-def run():
-    while True:
-        try:
-            diginet_orderbook = diginet.fetch_order_book('BTC/VND')
-            bitstamp_orderbook = bitstamp.fetch_order_book('BTC/USD')
-            compare_prices(bitstamp_orderbook, diginet_orderbook)
-        except Exception as ex:
-            logging.error(ex)
-        time.sleep(settings.interval)
+    def run(self):
+        while True:
+            try:
+                diginet_orderbook = self.diginet.fetch_order_book('BTC/VND')
+                bitstamp_orderbook = self.bitstamp.fetch_order_book('BTC/USD')
+                self.compare_prices(bitstamp_orderbook, diginet_orderbook)
+            except Exception as ex:
+                logging.error(ex)
+            time.sleep(settings.interval)
 
 
 if __name__ == '__main__':
-    run()
-
-# b_market = bitstamp.load_markets()
-# d_market = diginet.load_markets()
-#
-# print(d_market)
-
-# d_orderbook = diginet.fetch_order_book('BTC/VND')
-#
-# print(d_orderbook)
-# for ab in d_orderbook:
-#     print(d_orderbook[ab])
-# d_orderbook = bitstamp.fetch_order_book('BTC/USD')
-#
-# print(d_orderbook)
-# for ab in d_orderbook:
-#     print(d_orderbook[ab])
+    BackEnd.run()
 
--- run.py
+++ run.py
@@ -1,22 +1,45 @@
-import backend
+from backend import BackEnd
 import threading
 import logging
 import time
 import sys
 import os
 
-logging.info('Bot starting...')
+# set logging time to GMT
+logging.Formatter.converter = time.gmtime
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+fh = logging.FileHandler('diginetbot.log')
+fh.setLevel(logging.WARN)
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
+
+logger.info('Bot starting...')
 settings_mtime = os.path.getmtime('settings.py')
-backend_thread = threading.Thread(backend.run())
+backend_prog = BackEnd(logger=logger)
+backend_thread = threading.Thread(backend_prog.run())
 backend_thread.daemon = True
 backend_thread.start()
 
-logging.info('Bot started, running.')
+logger.info('Bot started, running.')
 while True:
     time.sleep(10)
     new_mtime = os.path.getmtime('settings.py')
     if new_mtime > settings_mtime:
-        logging.warning('Config changed, restarting bot')
+        logger.warning('Config changed, restarting bot')
         program = sys.executable
         os.execl(program, program, *sys.argv)
 
