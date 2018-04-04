d5f81206c2d5cc043b617c77e36362af376a6879
Huyen Ha <hathehuyen@gmail.com>
2018-04-04 19:23:39 +0700

    Auto load setting on changes

--- run.py
+++ run.py
@@ -1,11 +1,23 @@
 import backend
 import threading
+import logging
 import time
+import sys
+import os
 
+logging.info('Bot starting...')
+settings_mtime = os.path.getmtime('settings.py')
 backend_thread = threading.Thread(backend.run())
 backend_thread.daemon = True
 backend_thread.start()
 
+logging.info('Bot started, running.')
 while True:
     time.sleep(10)
+    new_mtime = os.path.getmtime('settings.py')
+    if new_mtime > settings_mtime:
+        logging.warning('Config changed, restarting bot')
+        program = sys.executable
+        os.execl(program, program, *sys.argv)
+
 
