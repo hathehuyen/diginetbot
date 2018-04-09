8a7cec6934508d6b6f96dbcf2e82080cb7c4fa98
Huyen Ha <hathehuyen@gmail.com>
2018-04-09 17:27:56 +0700

    Add log object

--- common/db.py
+++ common/db.py
@@ -7,6 +7,7 @@ db = client.diginetbot
 
 sessions_db = db.sessions
 users_db = db.users
+logs_db = db.logs
 
 
 class UserObj(object):
@@ -22,6 +23,11 @@ class UserObj(object):
             self.password = user['password']
             self.id = user['_id']
 
+    def insert_one(self, username: str, password: str):
+        self.id = users_db.insert_one({'username': username, 'password': generate_password_hash(password)}).inserted_id
+        self.username = username
+        self.password = generate_password_hash(password)
+
     def update_one(self, username: str, password: str):
         self.id = users_db.update_one({'username': username, 'password': generate_password_hash(password)}).inserted_id
         self.username = username
@@ -30,6 +36,7 @@ class UserObj(object):
 
 class SessionObj(object):
     def __init__(self):
+        self.id = None
         self.user_id = None
         self.status = None
         self.settings = None
@@ -37,6 +44,30 @@ class SessionObj(object):
     def find_one(self, session_id: str):
         session = sessions_db.find_one({'_id': objectid.ObjectId(session_id)})
         if session:
+            self.id = session['_id']
             self.user_id = session['user_id']
             self.status = session['status']
             self.settings = session['settings']
+
+    def save(self):
+        if self.id:
+            result = sessions_db.update_one({'_id': self.id, 'status': self.status, 'settings': self.settings})
+            return result.acknowledged
+        else:
+            return False
+
+
+class LogObj(object):
+    def __init__(self):
+        self.id = None
+        self.user_id = None
+        self.session_id = None
+        self.text = None
+
+    def save(self):
+        result = logs_db.update_one({'_id': self.id}, {"$set": {'user_id': self.user_id, 'session_id': self.session_id,
+                                                                'text': self.text}}, upsert=True)
+        return result.inserted_id
+
+    def get_logs(self, session_id):
+        return logs_db.find({'session_id': session_id})
